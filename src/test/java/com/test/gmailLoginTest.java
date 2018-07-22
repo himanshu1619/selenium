62f414639fdfa13062ccaf720c32f15becf0f54b
Himanshu Jain <HIJAIN@amdocs.com>
2018-07-22 17:28:31 +0530

    First Checkin

--- src/test/java/com/test/gmailLoginTest.java
+++ src/test/java/com/test/gmailLoginTest.java
@@ -1,9 +1,21 @@
 package com.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class gmailLoginTest {
 
+	@Test
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Jars\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+	
+	driver.get("http://www.gmail.com");
+	
+	
 
 	}
 
